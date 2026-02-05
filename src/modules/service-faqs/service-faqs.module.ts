8d18a07d50df3558fe63bba1e15b972aaccbe22f
zamirul <javshazib@gmail.com>
2026-02-05 18:56:16 +0600

    service faq entity added

--- src/modules/service-faqs/service-faqs.module.ts
+++ src/modules/service-faqs/service-faqs.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ServiceFaqsService } from './service-faqs.service';
 import { ServiceFaqsController } from './service-faqs.controller';
+import { ServiceFaq } from './entities/service-faq.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Service } from '../services/entities/service.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ServiceFaq, Service])],
   controllers: [ServiceFaqsController],
   providers: [ServiceFaqsService],
+  exports: [ServiceFaqsService],
 })
 export class ServiceFaqsModule {}
